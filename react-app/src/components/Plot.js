b559a06bc84befc60201ade964eb4d521bb438ac
emir-sehovic <smamela2@etf.unsa.ba>
2020-06-01 22:57:12 +0200

    boje

--- react-app/src/components/Plot.js
+++ react-app/src/components/Plot.js
@@ -2,9 +2,10 @@ import React, { useState, useEffect } from 'react';
 import 'react-vis/dist/style.css';
 import {XYPlot, MarkSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines } from 'react-vis';
 
-export default class Plot extends React.Component {
 
-    render() {
+export default class Plot extends React.Component {
+    constructor(props) {
+        super(props)
         const myPalette = [
             "#173f5f",
             "#20639b",
@@ -13,8 +14,13 @@ export default class Plot extends React.Component {
             "#ed553b",
             "#900c3f",
             "#b6cec7",
-            "#a37f6f"
+            "#a37f6f",
+            "#eacfac",
         ]
+        this.myPalette = shuffle(myPalette)
+    }
+    render() {
+        
         return (
             <XYPlot height={this.props.height} width={this.props.width - this.props.sidebarWidth}>
                 <XAxis />
@@ -32,10 +38,18 @@ export default class Plot extends React.Component {
                         key={JSON.stringify(this.props.centerPoints)} 
                         data={[center]}
                         size={8}
-                        color={myPalette[i]}
+                        color={this.myPalette[i]}
                     />
                 ))}
             </XYPlot>
         )
     }
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
 }
